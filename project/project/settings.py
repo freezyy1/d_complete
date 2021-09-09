9e164155950d087873248123fc982ae89e4bec20
Dmitrii <80770507+freezyy1@users.noreply.github.com>
2021-09-09 19:13:07 +0500

    fdfd

--- project/project/settings.py
+++ project/project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.flatpages',
+    'simpleapp',
 ]
 
 SITE_ID = 1
--- project/project/urls.py
+++ project/project/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('django.contrib.flatpages.urls')),
+    path('products/', include('simpleapp.urls')), # делаем так, чтобы все адреса из нашего приложения (simpleapp/urls.py) сами автоматически подключались когда мы их добавим.
 ]
