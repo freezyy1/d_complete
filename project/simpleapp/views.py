b56d1e542d2a1012b95e040f781ca7ba8dbbd47b
Dmitrii <80770507+freezyy1@users.noreply.github.com>
2021-09-11 12:17:32 +0500

    creating filters

--- project/simpleapp/views.py
+++ project/simpleapp/views.py
@@ -1,6 +1,7 @@
 # from django.shortcuts import render
 from django.views.generic import ListView, DetailView  # импортируем класс получения деталей объекта
 from .models import Product
+from datetime import datetime
 
 
 class ProductList(ListView):
@@ -8,6 +9,12 @@ class ProductList(ListView):
     template_name = 'products.html'
     context_object_name = 'products'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()  # добавим переменную текущей даты time_now
+        context[
+            'value1'] = None  # добавим ещё одну пустую переменную, чтобы на её примере посмотреть работу другого фильтра
+        return context
 
 # создаём представление в котором будет детали конкретного отдельного товара
 class ProductDetail(DetailView):
